a32ee2f0fb7e08beb734e066580af1e220810590
Andrej Suvorau <suvri4@gmail.com>
2021-02-16 17:11:12 +0300

    Window

---
+++ engine/system/include/system/window.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cstdint>
+
+struct SDL_Cursor;
+struct SDL_Window;
+
+namespace kw {
+
+struct WindowDescriptor {
+    const char* title;
+    uint32_t width;
+    uint32_t height;
+};
+
+enum class Cursor {
+    ARROW,
+    TEXT_INPUT,
+    RESIZE_ALL,
+    RESIZE_NS,
+    RESIZE_EW,
+    RESIZE_NESW,
+    RESIZE_NWSE,
+    HAND,
+    NOT_ALLOWED,
+    WAIT,
+};
+
+constexpr size_t CURSOR_COUNT = 10;
+
+class Window {
+public:
+    /** To create `Window` you must have an `EventLoop`. */
+    Window(const WindowDescriptor& descriptor);
+    ~Window();
+
+    const char* get_title() const;
+    void set_title(const char* title);
+
+    Cursor get_cursor() const;
+    void set_cursor(Cursor cursor);
+
+    bool is_cursor_shown() const;
+    void toggle_cursor(bool is_shown);
+
+    uint32_t get_width() const;
+    void set_width(uint32_t value);
+
+    uint32_t get_height() const;
+    void set_height(uint32_t value); 
+
+    uint32_t get_render_width() const;
+    uint32_t get_render_height() const;
+
+    SDL_Window* get_sdl_window() const;
+
+private:
+    SDL_Window* m_window;
+    SDL_Cursor* m_cursors[CURSOR_COUNT];
+};
+
+} // namespace kw
---
+++ engine/system/source/system/window.cpp
@@ -0,0 +1,103 @@
+#include <system/window.h>
+
+#include <core/error.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_vulkan.h>
+
+namespace kw {
+
+Window::Window(const WindowDescriptor& descriptor) {
+    m_window = SDL_CreateWindow(descriptor.title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, static_cast<int>(descriptor.width), static_cast<int>(descriptor.height), SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_VULKAN);
+    KW_ERROR(m_window != nullptr, "Failed to create window");
+
+    SDL_SetWindowData(m_window, "Window", this);
+
+    m_cursors[0] = SDL_GetDefaultCursor();
+    m_cursors[1] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_IBEAM);
+    m_cursors[2] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_SIZEALL);
+    m_cursors[3] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_SIZENS);
+    m_cursors[4] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_SIZEWE);
+    m_cursors[5] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_SIZENESW);
+    m_cursors[6] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_SIZENWSE);
+    m_cursors[7] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_HAND);
+    m_cursors[8] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_NO);
+    m_cursors[9] = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_WAIT);
+}
+
+Window::~Window() {
+    // m_cursors[i] is acquired from `SDL_GetDefaultCursor` and must not be freed.
+    for (size_t i = 1; i < CURSOR_COUNT; i++) {
+        SDL_FreeCursor(m_cursors[i]);
+    }
+
+    SDL_DestroyWindow(m_window);
+}
+
+const char* Window::get_title() const {
+    return SDL_GetWindowTitle(m_window);
+}
+
+void Window::set_title(const char* title) {
+    SDL_SetWindowTitle(m_window, title);
+}
+
+Cursor Window::get_cursor() const {
+    SDL_Cursor* cursor = SDL_GetCursor();
+    for (size_t i = 0; i < CURSOR_COUNT; i++) {
+        if (cursor == m_cursors[i]) {
+            return static_cast<Cursor>(i);
+        }
+    }
+    return Cursor::ARROW;
+}
+
+void Window::set_cursor(Cursor cursor) {
+    SDL_SetCursor(m_cursors[static_cast<size_t>(cursor)]);
+}
+
+bool Window::is_cursor_shown() const {
+    return SDL_ShowCursor(-1) == 1;
+}
+
+void Window::toggle_cursor(bool is_shown) {
+    SDL_ShowCursor(is_shown ? 1 : 0);
+}
+
+uint32_t Window::get_width() const {
+    uint32_t result;
+    SDL_GetWindowSize(m_window, reinterpret_cast<int*>(&result), nullptr);
+    return result;
+}
+
+void Window::set_width(uint32_t value) {
+    SDL_SetWindowSize(m_window, static_cast<int>(value), get_height());
+}
+
+uint32_t Window::get_height() const {
+    uint32_t result;
+    SDL_GetWindowSize(m_window, nullptr, reinterpret_cast<int*>(&result));
+    return result;
+}
+
+void Window::set_height(uint32_t value) {
+    SDL_SetWindowSize(m_window, get_width(), static_cast<int>(value));
+}
+
+uint32_t Window::get_render_width() const {
+    uint32_t result;
+    SDL_Vulkan_GetDrawableSize(m_window, reinterpret_cast<int*>(&result), nullptr);
+    return result;
+}
+
+uint32_t Window::get_render_height() const {
+    uint32_t result;
+    SDL_Vulkan_GetDrawableSize(m_window, nullptr, reinterpret_cast<int*>(&result));
+    return result;
+}
+
+SDL_Window* Window::get_sdl_window() const {
+    return m_window;
+}
+
+} // namespace kw
