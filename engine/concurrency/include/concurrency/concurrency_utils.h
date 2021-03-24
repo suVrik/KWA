17d1a7038b820047b1fd67b442dc469e689877b2
Andrej Suvorau <suvri4@gmail.com>
2021-03-25 00:28:08 +0300

    Set thread name

--- engine/concurrency/include/concurrency/concurrency_utils.h
+++ engine/concurrency/include/concurrency/concurrency_utils.h
@@ -7,4 +7,7 @@ namespace kw::ConcurrencyUtils {
 /** Each enabled bit allows the thread to run on the corresponding CPU core. */
 void set_thread_affinity(std::thread& thread, uint64_t affinity_mask);
 
+/** Set thread name for debugging convenience. */
+void set_thread_name(std::thread& thread, const char* name);
+
 } // namespace kw::ConcurrencyUtils
--- engine/concurrency/source/concurrency/concurrency_utils.cpp
+++ engine/concurrency/source/concurrency/concurrency_utils.cpp
@@ -1,11 +1,19 @@
 #include "concurrency/concurrency_utils.h"
 
+#include <processthreadsapi.h>
 #include <Windows.h>
 
 namespace kw::ConcurrencyUtils {
 
 void set_thread_affinity(std::thread& thread, uint64_t affinity_mask) {
     SetThreadAffinityMask(thread.native_handle(), static_cast<DWORD_PTR>(affinity_mask));
+}
+
+void set_thread_name(std::thread& thread, const char* name) {
+    WCHAR wide_name[64];
+    if (MultiByteToWideChar(CP_UTF8, 0, name, -1, wide_name, std::size(wide_name)) > 0) {
+        SetThreadDescription(thread.native_handle(), wide_name);
+    }
 }
 
 } // namespace kw::ConcurrencyUtils
