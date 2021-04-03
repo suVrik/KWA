6fef42976c7bf7723e12c9d34d66a3fde39746b1
Andrej Suvorau <suvri4@gmail.com>
2021-04-03 12:48:55 +0300

    Move texture format to Render

--- engine/render/include/render/frame_graph.h
+++ engine/render/include/render/frame_graph.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <cstdint>
+#include "render/render.h"
 
 namespace kw {
 
@@ -22,79 +22,6 @@ enum class Semantic {
 };
 
 constexpr size_t SEMANTIC_COUNT = 8;
-
-enum class TextureFormat {
-    UNKNOWN,
-    R8_SINT,
-    R8_SNORM,
-    R8_UINT,
-    R8_UNORM,
-    RG8_SINT,
-    RG8_SNORM,
-    RG8_UINT,
-    RG8_UNORM,
-    RGBA8_SINT,
-    RGBA8_SNORM,
-    RGBA8_UINT,
-    RGBA8_UNORM,
-    RGBA8_UNORM_SRGB,
-    R16_FLOAT,
-    R16_SINT,
-    R16_SNORM,
-    R16_UINT,
-    R16_UNORM,
-    RG16_FLOAT,
-    RG16_SINT,
-    RG16_SNORM,
-    RG16_UINT,
-    RG16_UNORM,
-    RGBA16_FLOAT,
-    RGBA16_SINT,
-    RGBA16_SNORM,
-    RGBA16_UINT,
-    RGBA16_UNORM,
-    R32_FLOAT,
-    R32_SINT,
-    R32_UINT,
-    RG32_FLOAT,
-    RG32_SINT,
-    RG32_UINT,
-    RGBA32_FLOAT,
-    RGBA32_SINT,
-    RGBA32_UINT,
-    BGRA8_UNORM,
-    BGRA8_UNORM_SRGB,
-    D16_UNORM,
-    D24_UNORM_S8_UINT,
-    D32_FLOAT,
-    D32_FLOAT_S8X24_UINT,
-    BC1_UNORM,
-    BC1_UNORM_SRGB,
-    BC2_UNORM,
-    BC2_UNORM_SRGB,
-    BC3_UNORM,
-    BC3_UNORM_SRGB,
-    BC4_SNORM,
-    BC4_UNORM,
-    BC5_SNORM,
-    BC5_UNORM,
-    BC6H_SF16,
-    BC6H_UF16,
-    BC7_UNORM,
-    BC7_UNORM_SRGB,
-};
-
-constexpr size_t TEXTURE_FORMAT_COUNT = 58;
-
-namespace TextureFormatUtils {
-
-bool is_depth_stencil(TextureFormat format);
-bool is_compressed(TextureFormat format);
-
-/** For compressed types returns 0. */
-size_t get_pixel_size(TextureFormat format);
-
-} // namespace TextureFormatUtils
 
 struct AttributeDescriptor {
     Semantic semantic;
--- engine/render/source/render/render.cpp
+++ engine/render/source/render/render.cpp
@@ -3,9 +3,214 @@
 
 #include <core/error.h>
 
-#include <debug/assert.h>
+#include <array>
 
 namespace kw {
+
+namespace TextureFormatUtils {
+
+static const bool IS_DEPTH_STENCIL[] = {
+    false, // UNKNOWN
+    false, // R8_SINT
+    false, // R8_SNORM
+    false, // R8_UINT
+    false, // R8_UNORM
+    false, // RG8_SINT
+    false, // RG8_SNORM
+    false, // RG8_UINT
+    false, // RG8_UNORM
+    false, // RGBA8_SINT
+    false, // RGBA8_SNORM
+    false, // RGBA8_UINT
+    false, // RGBA8_UNORM
+    false, // RGBA8_UNORM_SRGB
+    false, // R16_FLOAT
+    false, // R16_SINT
+    false, // R16_SNORM
+    false, // R16_UINT
+    false, // R16_UNORM
+    false, // RG16_FLOAT
+    false, // RG16_SINT
+    false, // RG16_SNORM
+    false, // RG16_UINT
+    false, // RG16_UNORM
+    false, // RGBA16_FLOAT
+    false, // RGBA16_SINT
+    false, // RGBA16_SNORM
+    false, // RGBA16_UINT
+    false, // RGBA16_UNORM
+    false, // R32_FLOAT
+    false, // R32_SINT
+    false, // R32_UINT
+    false, // RG32_FLOAT
+    false, // RG32_SINT
+    false, // RG32_UINT
+    false, // RGBA32_FLOAT
+    false, // RGBA32_SINT
+    false, // RGBA32_UINT
+    false, // BGRA8_UNORM
+    false, // BGRA8_UNORM_SRGB
+    true,  // D16_UNORM
+    true,  // D24_UNORM_S8_UINT
+    true,  // D32_FLOAT
+    true,  // D32_FLOAT_S8X24_UINT
+    false, // BC1_UNORM
+    false, // BC1_UNORM_SRGB
+    false, // BC2_UNORM
+    false, // BC2_UNORM_SRGB
+    false, // BC3_UNORM
+    false, // BC3_UNORM_SRGB
+    false, // BC4_SNORM
+    false, // BC4_UNORM
+    false, // BC5_SNORM
+    false, // BC5_UNORM
+    false, // BC6H_SF16
+    false, // BC6H_UF16
+    false, // BC7_UNORM
+    false, // BC7_UNORM_SRGB
+};
+
+static_assert(std::size(IS_DEPTH_STENCIL) == TEXTURE_FORMAT_COUNT);
+
+bool is_depth_stencil(TextureFormat format) {
+    return IS_DEPTH_STENCIL[static_cast<size_t>(format)];
+}
+
+static const bool IS_COMPRESSED[] = {
+    false, // UNKNOWN
+    false, // R8_SINT
+    false, // R8_SNORM
+    false, // R8_UINT
+    false, // R8_UNORM
+    false, // RG8_SINT
+    false, // RG8_SNORM
+    false, // RG8_UINT
+    false, // RG8_UNORM
+    false, // RGBA8_SINT
+    false, // RGBA8_SNORM
+    false, // RGBA8_UINT
+    false, // RGBA8_UNORM
+    false, // RGBA8_UNORM_SRGB
+    false, // R16_FLOAT
+    false, // R16_SINT
+    false, // R16_SNORM
+    false, // R16_UINT
+    false, // R16_UNORM
+    false, // RG16_FLOAT
+    false, // RG16_SINT
+    false, // RG16_SNORM
+    false, // RG16_UINT
+    false, // RG16_UNORM
+    false, // RGBA16_FLOAT
+    false, // RGBA16_SINT
+    false, // RGBA16_SNORM
+    false, // RGBA16_UINT
+    false, // RGBA16_UNORM
+    false, // R32_FLOAT
+    false, // R32_SINT
+    false, // R32_UINT
+    false, // RG32_FLOAT
+    false, // RG32_SINT
+    false, // RG32_UINT
+    false, // RGBA32_FLOAT
+    false, // RGBA32_SINT
+    false, // RGBA32_UINT
+    false, // BGRA8_UNORM
+    false, // BGRA8_UNORM_SRGB
+    false, // D16_UNORM
+    false, // D24_UNORM_S8_UINT
+    false, // D32_FLOAT
+    false, // D32_FLOAT_S8X24_UINT
+    true,  // BC1_UNORM
+    true,  // BC1_UNORM_SRGB
+    true,  // BC2_UNORM
+    true,  // BC2_UNORM_SRGB
+    true,  // BC3_UNORM
+    true,  // BC3_UNORM_SRGB
+    true,  // BC4_SNORM
+    true,  // BC4_UNORM
+    true,  // BC5_SNORM
+    true,  // BC5_UNORM
+    true,  // BC6H_SF16
+    true,  // BC6H_UF16
+    true,  // BC7_UNORM
+    true,  // BC7_UNORM_SRGB
+};
+
+static_assert(std::size(IS_COMPRESSED) == TEXTURE_FORMAT_COUNT);
+
+bool is_compressed(TextureFormat format) {
+    return IS_COMPRESSED[static_cast<size_t>(format)];
+}
+
+static const size_t PIXEL_SIZE[] = {
+    0,  // UNKNOWN
+    1,  // R8_SINT
+    1,  // R8_SNORM
+    1,  // R8_UINT
+    1,  // R8_UNORM
+    2,  // RG8_SINT
+    2,  // RG8_SNORM
+    2,  // RG8_UINT
+    2,  // RG8_UNORM
+    4,  // RGBA8_SINT
+    4,  // RGBA8_SNORM
+    4,  // RGBA8_UINT
+    4,  // RGBA8_UNORM
+    4,  // RGBA8_UNORM_SRGB
+    2,  // R16_FLOAT
+    2,  // R16_SINT
+    2,  // R16_SNORM
+    2,  // R16_UINT
+    2,  // R16_UNORM
+    4,  // RG16_FLOAT
+    4,  // RG16_SINT
+    4,  // RG16_SNORM
+    4,  // RG16_UINT
+    4,  // RG16_UNORM
+    8,  // RGBA16_FLOAT
+    8,  // RGBA16_SINT
+    8,  // RGBA16_SNORM
+    8,  // RGBA16_UINT
+    8,  // RGBA16_UNORM
+    4,  // R32_FLOAT
+    4,  // R32_SINT
+    4,  // R32_UINT
+    8,  // RG32_FLOAT
+    8,  // RG32_SINT
+    8,  // RG32_UINT
+    16, // RGBA32_FLOAT
+    16, // RGBA32_SINT
+    16, // RGBA32_UINT
+    4,  // BGRA8_UNORM
+    4,  // BGRA8_UNORM_SRGB
+    2,  // D16_UNORM
+    4,  // D24_UNORM_S8_UINT
+    4,  // D32_FLOAT
+    8,  // D32_FLOAT_S8X24_UINT
+    0,  // BC1_UNORM
+    0,  // BC1_UNORM_SRGB
+    0,  // BC2_UNORM
+    0,  // BC2_UNORM_SRGB
+    0,  // BC3_UNORM
+    0,  // BC3_UNORM_SRGB
+    0,  // BC4_SNORM
+    0,  // BC4_UNORM
+    0,  // BC5_SNORM
+    0,  // BC5_UNORM
+    0,  // BC6H_SF16
+    0,  // BC6H_UF16
+    0,  // BC7_UNORM
+    0,  // BC7_UNORM_SRGB
+};
+
+static_assert(std::size(PIXEL_SIZE) == TEXTURE_FORMAT_COUNT);
+
+size_t get_pixel_size(TextureFormat format) {
+    return PIXEL_SIZE[static_cast<size_t>(format)];
+}
+
+} // namespace TextureFormatUtils
 
 Render* Render::create_instance(const RenderDescriptor& descriptor) {
     KW_ASSERT(descriptor.memory_resource != nullptr);
