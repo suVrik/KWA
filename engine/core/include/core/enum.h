c2830fcf9401d34dfe542a9df98b786d2d6d2b9e
Andrej Suvorau <suvri4@gmail.com>
2021-04-09 12:21:36 +0300

    Bitwise operators declaration for private enum classes

--- engine/core/include/core/enum.h
+++ engine/core/include/core/enum.h
@@ -2,6 +2,15 @@
 
 #include <cstdint>
 
+#define KW_DECLARE_ENUM_BITMASK(type)                                                  \
+friend constexpr type operator|(type lhs, type rhs);                                   \
+friend constexpr type operator&(type lhs, type rhs);                                   \
+friend constexpr type operator^(type lhs, type rhs);                                   \
+friend constexpr type& operator|=(type& lhs, type rhs);                                \
+friend constexpr type& operator&=(type& lhs, type rhs);                                \
+friend constexpr type& operator^=(type& lhs, type rhs);                                \
+friend constexpr type operator~(type e)
+
 #define KW_DEFINE_ENUM_BITMASK(type)                                                   \
 constexpr type operator|(type lhs, type rhs) {                                         \
     return static_cast<type>(static_cast<uint32_t>(lhs) | static_cast<uint32_t>(rhs)); \
@@ -26,4 +35,5 @@ constexpr type& operator^=(type& lhs, type rhs) {                               
 }                                                                                      \
 constexpr type operator~(type e) {                                                     \
     return static_cast<type>(~static_cast<uint32_t>(e));                               \
-}
+}                                                                                      \
+static_assert(true)
