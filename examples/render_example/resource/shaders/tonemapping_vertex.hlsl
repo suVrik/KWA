b1ea161ecbb1c2405a6645e8ee2009a5172b7634
Andrej Suvorau <suvri4@gmail.com>
2021-05-19 14:23:15 +0300

    Remove render module's shaders from render example

--- examples/render_example/resource/shaders/tonemapping_vertex.hlsl
+++
@@ -1,9 +0,0 @@
-struct TonemappingData {
-    float4x4 view_projection;
-};
-
-[[vk::push_constant]] TonemappingData tonemapping_data;
-
-float4 main(float4 position : POSITION) : SV_POSITION {
-    return position;
-}
