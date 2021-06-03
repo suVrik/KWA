3b3124515768b729f8c8cbc5c4e9b05531b2a770
Andrej Suvorau <suvri4@gmail.com>
2021-06-03 13:49:12 +0300

    Spinlock CPU hint

--- engine/core/source/core/concurrency/spinlock.cpp
+++ engine/core/source/core/concurrency/spinlock.cpp
@@ -1,9 +1,13 @@
 #include "core/concurrency/spinlock.h"
 
+#include <emmintrin.h>
+
 namespace kw {
 
 void Spinlock::lock() {
-    while (m_is_locked.test_and_set(std::memory_order_acquire));
+    while (m_is_locked.test_and_set(std::memory_order_acquire)) {
+        _mm_pause();
+    }
 }
 
 bool Spinlock::try_lock() {
