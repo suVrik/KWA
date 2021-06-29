3f42634d98e48ba7c7f7a0a24241fbdddf9caf63
Andrej Suvorau <suvri4@gmail.com>
2021-06-29 11:53:59 +0300

    Geometry

---
+++ engine/render/include/render/geometry/geometry.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <core/math/aabbox.h>
+
+namespace kw {
+
+class IndexBuffer;
+class VertexBuffer;
+
+class Geometry {
+public:
+    Geometry()
+        : m_vertex_buffer(nullptr)
+        , m_index_buffer(nullptr)
+        , m_index_count(0)
+        , m_bounds{}
+    {
+    }
+    
+    Geometry(VertexBuffer* vertex_buffer, IndexBuffer* index_buffer, uint32_t index_count, const aabbox& bounds)
+        : m_vertex_buffer(vertex_buffer)
+        , m_index_buffer(index_buffer)
+        , m_index_count(index_count)
+        , m_bounds(bounds)
+    {
+    }
+
+    VertexBuffer* get_vertex_buffer() const {
+        return m_vertex_buffer;
+    }
+
+    IndexBuffer* get_index_buffer() const {
+        return m_index_buffer;
+    }
+
+    uint32_t get_index_count() const {
+        return m_index_count;
+    }
+
+    const aabbox& get_bounds() const {
+        return m_bounds;
+    }
+
+private:
+    VertexBuffer* m_vertex_buffer;
+    IndexBuffer* m_index_buffer;
+    uint32_t m_index_count;
+    aabbox m_bounds;
+};
+
+} // namespace kw
