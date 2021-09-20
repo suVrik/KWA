1323a499b9a5a10c3f338956f134a17e3e427bbf
Andrej Suvorau <suvri4@gmail.com>
2021-09-20 03:28:12 +0300

    Animated geometry primitive is an abstract base class now

--- engine/render/source/render/animation/animated_geometry_primitive.cpp
+++ engine/render/source/render/animation/animated_geometry_primitive.cpp
@@ -1,15 +1,9 @@
 #include "render/animation/animated_geometry_primitive.h"
-#include "render/animation/animation_manager.h"
 #include "render/animation/animation_player.h"
 #include "render/geometry/geometry.h"
-#include "render/geometry/geometry_manager.h"
 #include "render/geometry/skeleton.h"
-#include "render/material/material_manager.h"
-#include "render/scene/render_primitive_reflection.h"
 
 #include <core/debug/assert.h>
-#include <core/io/markdown.h>
-#include <core/io/markdown_utils.h>
 
 #include <atomic>
 
@@ -18,33 +12,12 @@ namespace kw {
 // Declared in `render/acceleration_structure/acceleration_structure_primitive.cpp`.
 extern std::atomic_uint64_t acceleration_structure_counter;
 
-UniquePtr<Primitive> AnimatedGeometryPrimitive::create_from_markdown(PrimitiveReflection& reflection, const ObjectNode& node) {
-    RenderPrimitiveReflection& render_reflection = dynamic_cast<RenderPrimitiveReflection&>(reflection);
-
-    StringNode& animation_node = node["animation"].as<StringNode>();
-    StringNode& geometry_node = node["geometry"].as<StringNode>();
-    StringNode& material_node = node["material"].as<StringNode>();
-    StringNode& shadow_material_node = node["shadow_material"].as<StringNode>();
-
-    SharedPtr<Animation> animation = render_reflection.animation_manager.load(animation_node.get_value().c_str());
-    SharedPtr<Geometry> geometry = render_reflection.geometry_manager.load(geometry_node.get_value().c_str());
-    SharedPtr<Material> material = render_reflection.material_manager.load(material_node.get_value().c_str());
-    SharedPtr<Material> shadow_material = render_reflection.material_manager.load(shadow_material_node.get_value().c_str());
-    transform local_transform = MarkdownUtils::transform_from_markdown(node["local_transform"]);
-
-    return static_pointer_cast<Primitive>(allocate_unique<AnimatedGeometryPrimitive>(
-        reflection.memory_resource, reflection.memory_resource, animation, geometry, material, shadow_material, local_transform
-    ));
-}
-
-AnimatedGeometryPrimitive::AnimatedGeometryPrimitive(MemoryResource& memory_resource, SharedPtr<Animation> animation,
-                                                     SharedPtr<Geometry> geometry, SharedPtr<Material> material,
-                                                     SharedPtr<Material> shadow_material, const transform& local_transform)
+AnimatedGeometryPrimitive::AnimatedGeometryPrimitive(MemoryResource& memory_resource, SharedPtr<Geometry> geometry,
+                                                     SharedPtr<Material> material, SharedPtr<Material> shadow_material,
+                                                     const transform& local_transform)
     : GeometryPrimitive(geometry, material, shadow_material, local_transform)
     , m_animation_player(nullptr)
     , m_skeleton_pose(memory_resource)
-    , m_animation(std::move(animation))
-    , m_animation_time(0.f)
     , m_animation_speed(1.f)
 {
 }
@@ -53,8 +26,6 @@ AnimatedGeometryPrimitive::AnimatedGeometryPrimitive(const AnimatedGeometryPrimi
     : GeometryPrimitive(other)
     , m_animation_player(nullptr)
     , m_skeleton_pose(other.m_skeleton_pose)
-    , m_animation(other.m_animation)
-    , m_animation_time(other.m_animation_time)
     , m_animation_speed(other.m_animation_speed)
 {
     KW_ASSERT(
@@ -63,12 +34,10 @@ AnimatedGeometryPrimitive::AnimatedGeometryPrimitive(const AnimatedGeometryPrimi
     );
 }
 
-AnimatedGeometryPrimitive::AnimatedGeometryPrimitive(AnimatedGeometryPrimitive&& other)
+AnimatedGeometryPrimitive::AnimatedGeometryPrimitive(AnimatedGeometryPrimitive&& other) noexcept
     : GeometryPrimitive(std::move(other))
     , m_animation_player(nullptr)
     , m_skeleton_pose(std::move(other.m_skeleton_pose))
-    , m_animation(std::move(other.m_animation))
-    , m_animation_time(other.m_animation_time)
     , m_animation_speed(other.m_animation_speed)
 {
     KW_ASSERT(
@@ -84,41 +53,41 @@ AnimatedGeometryPrimitive::~AnimatedGeometryPrimitive() {
 }
 
 AnimatedGeometryPrimitive& AnimatedGeometryPrimitive::operator=(const AnimatedGeometryPrimitive& other) {
-    GeometryPrimitive::operator=(other);
+    if (&other != this) {
+        GeometryPrimitive::operator=(other);
 
-    if (m_animation_player != nullptr) {
-        m_animation_player->remove(*this);
+        if (m_animation_player != nullptr) {
+            m_animation_player->remove(*this);
+        }
+
+        m_skeleton_pose = other.m_skeleton_pose;
+        m_animation_speed = other.m_animation_speed;
+
+        KW_ASSERT(
+            other.m_animation_player == nullptr,
+            "Copying animated geometry primitives with an animation player assigned is not allowed."
+        );
     }
-
-    m_skeleton_pose = other.m_skeleton_pose;
-    m_animation = other.m_animation;
-    m_animation_time = other.m_animation_time;
-    m_animation_speed = other.m_animation_speed;
-
-    KW_ASSERT(
-        other.m_animation_player == nullptr,
-        "Copying animated geometry primitives with an animation player assigned is not allowed."
-    );
 
     return *this;
 }
 
-AnimatedGeometryPrimitive& AnimatedGeometryPrimitive::operator=(AnimatedGeometryPrimitive&& other) {
-    GeometryPrimitive::operator=(std::move(other));
+AnimatedGeometryPrimitive& AnimatedGeometryPrimitive::operator=(AnimatedGeometryPrimitive&& other) noexcept {
+    if (&other != this) {
+        GeometryPrimitive::operator=(std::move(other));
 
-    if (m_animation_player != nullptr) {
-        m_animation_player->remove(*this);
+        if (m_animation_player != nullptr) {
+            m_animation_player->remove(*this);
+        }
+
+        m_skeleton_pose = std::move(other.m_skeleton_pose);
+        m_animation_speed = other.m_animation_speed;
+
+        KW_ASSERT(
+            other.m_animation_player == nullptr,
+            "Copying animated geometry primitives with an animation player assigned is not allowed."
+        );
     }
-
-    m_skeleton_pose = std::move(other.m_skeleton_pose);
-    m_animation = std::move(other.m_animation);
-    m_animation_time = other.m_animation_time;
-    m_animation_speed = other.m_animation_speed;
-
-    KW_ASSERT(
-        other.m_animation_player == nullptr,
-        "Copying animated geometry primitives with an animation player assigned is not allowed."
-    );
 
     return *this;
 }
@@ -137,24 +106,8 @@ SkeletonPose& AnimatedGeometryPrimitive::get_skeleton_pose() {
     return m_skeleton_pose;
 }
 
-const SharedPtr<Animation>& AnimatedGeometryPrimitive::get_animation() const {
-    return m_animation;
-}
-
-void AnimatedGeometryPrimitive::set_animation(SharedPtr<Animation> animation) {
-    m_animation = std::move(animation);
-}
-
 Vector<float4x4> AnimatedGeometryPrimitive::get_model_space_joint_matrices(MemoryResource& memory_resource) {
     return Vector<float4x4>(m_skeleton_pose.get_model_space_matrices(), memory_resource);
-}
-
-float AnimatedGeometryPrimitive::get_animation_time() const {
-    return m_animation_time;
-}
-
-void AnimatedGeometryPrimitive::set_animation_time(float value) {
-    m_animation_time = value;
 }
 
 float AnimatedGeometryPrimitive::get_animation_speed() const {
@@ -165,19 +118,16 @@ void AnimatedGeometryPrimitive::set_animation_speed(float value) {
     m_animation_speed = value;
 }
 
-UniquePtr<Primitive> AnimatedGeometryPrimitive::clone(MemoryResource& memory_resource) const {
-    return static_pointer_cast<Primitive>(allocate_unique<AnimatedGeometryPrimitive>(memory_resource, *this));
-}
-
 void AnimatedGeometryPrimitive::geometry_loaded() {
     KW_ASSERT(get_geometry() && get_geometry()->is_loaded(), "Geometry must be loaded.");
 
     const Skeleton* skeleton = get_geometry()->get_skeleton();
     if (skeleton != nullptr) {
         for (uint32_t i = 0; i < skeleton->get_joint_count(); i++) {
-            m_skeleton_pose.set_joint_space_matrix(i, skeleton->get_bind_matrix(i));
+            m_skeleton_pose.set_joint_space_transform(i, skeleton->get_bind_transform(i));
         }
         m_skeleton_pose.build_model_space_matrices(*skeleton);
+        m_skeleton_pose.apply_inverse_bind_matrices(*skeleton);
     }
 
     GeometryPrimitive::geometry_loaded();
